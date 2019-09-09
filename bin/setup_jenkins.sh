a352c57d6dca1861822d343eeb67e806df37c471
Behnam Hajian <b.hajian@gmail.com>
2019-09-09 14:29:04 -0400

    fixes the first step

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -13,7 +13,7 @@ CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 # Set up Jenkins with sufficient resources
-# TBD
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
 
 # Create custom agent container image with skopeo
 # TBD
